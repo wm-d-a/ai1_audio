162e4bc5961d522922a38c14c52913bbdfbaeaa9
Damir Nizamov <dnizamov@lucrum-analytics.ru>
2023-10-17 13:16:24 +0300

    init commit

---
+++ angry.py
@@ -0,0 +1,41 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchaudio
+from transformers import AutoConfig, Wav2Vec2FeatureExtractor  # , Wav2Vec2ForSpeechClassification
+from src.models import Wav2Vec2ForSpeechClassification  # , HubertForSpeechClassification
+import librosa
+# import IPython.display as ipd
+import numpy as np
+import pandas as pd
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model_name_or_path = "harshit345/xlsr-wav2vec-speech-emotion-recognition"
+config = AutoConfig.from_pretrained(model_name_or_path)
+feature_extractor = Wav2Vec2FeatureExtractor.from_pretrained(model_name_or_path)
+sampling_rate = feature_extractor.sampling_rate
+model = Wav2Vec2ForSpeechClassification.from_pretrained(model_name_or_path).to(device)
+
+
+def speech_file_to_array_fn(path, sampling_rate):
+    speech_array, _sampling_rate = torchaudio.load(path)
+    resampler = torchaudio.transforms.Resample(_sampling_rate)
+    speech = resampler(speech_array).squeeze().numpy()
+    return speech
+
+
+def predict(path, sampling_rate):
+    speech = speech_file_to_array_fn(path, sampling_rate)
+    inputs = feature_extractor(speech, sampling_rate=sampling_rate, return_tensors="pt", padding=True)
+    inputs = {key: inputs[key].to(device) for key in inputs}
+    with torch.no_grad():
+        logits = model(**inputs).logits
+    scores = F.softmax(logits, dim=1).detach().cpu().numpy()[0]
+    outputs = [{"Emotion": config.id2label[i], "Score": f"{round(score * 100, 3):.1f}%"} for i, score in
+               enumerate(scores)]
+    return outputs
+
+
+path = 'smeh-jirinovskogo.wav'
+outputs = predict(path, sampling_rate)
+print(outputs)
